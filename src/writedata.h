928270e300d58d07724ee5102a1aec2520993861
Philippe Miron <philippe.miron@polymtl.ca>
2014-02-19 11:07:31 -0500

    bad dropbox sync at one point those got deleted.

---
+++ src/writedata.h
@@ -0,0 +1,26 @@
+#ifndef _writedata_
+#define _writedata_
+
+#include "ftle.h"
+#include "parameter.h"
+#include "flowmap.h"
+#include "lyapunov.h"
+
+class writedata
+{
+public:
+   writedata(parameter* objpara, flowmap* objfm, lyapunov* objftle);
+   virtual ~writedata(void);
+
+private:
+    void write_ftle(std::string file);
+    int nx;
+	int ny;	
+	int nz;
+	std::vector<double> x;
+	std::vector<double> y;
+	std::vector<double> z;
+	double*** ftle;
+};
+
+#endif
